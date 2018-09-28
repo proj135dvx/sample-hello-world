51211e8ae4acd9d2fc342f6d24a0f0405dece1d4
root <root@vultr.guest>
2018-09-28 07:26:43 +0000

    Initial commit

---
+++ application/views/helloview.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $mega_title ?></title>
+</head>
+<body>
+<div id="container">
+    <h1><?php echo $title ?></h1>
+    <div id="body">
+        <p><?php echo $message ?></p>
+    </div>
+</div>
+</body>
+</html>
